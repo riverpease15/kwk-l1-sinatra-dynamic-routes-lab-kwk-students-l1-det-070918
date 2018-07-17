aa5c498da376a1b38a3c6d7b0ef5593b75467b39
River Pease <riverpease15@gmail.com>
2018-07-17 19:31:31 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,36 +3,36 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
   get '/reversename/:name' do
-    @name = name.reverse
+    @name = params[:name].reverse
     "#{@name}"
   end
 
   get '/square/:number' do
-    @number *= number.to_i
-    "#{@number.to_s}"
+    num = params[:number]
+    (num.to_i**2).to_s
   end
   
   get '/say/:number/:phrase' do
-    @number = number
-    @phrase = phrase
+    @number = params[:number]
+    @phrase = params[:phrase]
     until number == number
       "#{@phrase}"
     end
   end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @word1 = word1
-    @word2 = word2
-    @word3 = word3
-    @word4 = word4
-    @word5 = word5
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
     "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}"
   end
   
   gets '/:operation/:number1/:number2' do
-    @operation = operation
-    @number1 = number1.to_i
-    @number2 = number2.to_i
+    @operation = params[:operation]
+    @number1 = params[:number1.to_i]
+    @number2 = params[:number2.to_i]
     
   end
 end
